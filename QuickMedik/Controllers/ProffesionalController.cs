5ae716e2f946cb6ee4ec813c6f79b3dd99379fc8
Gabrielius Salyga <gabrielius.salyga@gmail.com>
2023-11-02 17:07:16 +0200

    ?
    merge

--- QuickMedik/Controllers/ProffesionalController.cs
+++ QuickMedik/Controllers/ProffesionalController.cs
@@ -1,6 +1,27 @@
-﻿namespace QuickMedik.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickMedik.Model;
+using QuickMedik.Data;
+using Microsoft.EntityFrameworkCore;
+namespace QuickMedik.Controllers
 {
-    public class ProffesionalController
+    [Route("api/ProffesionalAPI")]
+    [ApiController]
+    public class ProffesionalController : ControllerBase
     {
+
+        private readonly DataBaseContext _dbContext;
+
+        public ProffesionalController(DataBaseContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        /*[HttpPost]
+        public async Task<IActionResult> Create(string name, string surname, string email, string phone, string workSpaceId)
+        {
+            string Id = "belekas";
+
+
+        }*/
     }
 }
--- QuickMedik/Model/Proffesional.cs
+++ QuickMedik/Model/Proffesional.cs
@@ -2,13 +2,13 @@
 {
     public class Proffesional : User
     {
-        public float Rating { get; private set; }
+        public float Rating { get; private set; } //by default 0, turi sekti kiek zmoniu reitingu uzdejo ir funkcijos calculateRating()
 
-        public List<Service> Specialties { get; private set; }
+        public List<Service> Specialties { get; private set; } //Pradzioje tuscias, todel konstruktoriuje nereikia. Prisiregistraves daktaras gales tik tada susirasyti visus specialties
 
-        public string WorkspaceId { get; private set; }
+        public string WorkspaceId { get; private set; } //idk ar registruojantis ar po registracijos sita pasirenka
         
-        public List<Time> Times { get; private set; }
+        public List<Time> Times { get; private set; } //sito irgi nereikai konstruktoriuje, nes tik prisiregistraves gales 
 
         public Location Location { get; private set; }
 
