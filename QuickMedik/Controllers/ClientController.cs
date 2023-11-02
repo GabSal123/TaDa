fed7eb0b02d61af805467d423c568fc27690a926
Gabrielius Salyga <gabrielius.salyga@gmail.com>
2023-11-02 16:14:41 +0200

    Added controller
    Added Controller

---
+++ QuickMedik/Controllers/ClientController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickMedik.Model;
+using QuickMedik.Data;
+using Microsoft.EntityFrameworkCore;
+namespace QuickMedik.Controllers
+{
+
+    [Route("api/ClientAPI")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly DataBaseContext _dbContext;
+
+        public ClientController(DataBaseContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            Client client = new Client
+            {
+                
+            };
+        }
+    }
+}
